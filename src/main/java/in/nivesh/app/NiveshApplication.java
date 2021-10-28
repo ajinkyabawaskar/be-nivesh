ca4d2e16eaa5415ca06085fc148e57d6deaaa8f6
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2021-10-28 17:36:42 +0530

    first commit

---
+++ src/main/java/in/nivesh/app/NiveshApplication.java
@@ -0,0 +1,13 @@
+package in.nivesh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiveshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiveshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nivesh/app/NiveshApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nivesh.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiveshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
